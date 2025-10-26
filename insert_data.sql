7bdfb1254657bfe88c64da3da56ba9e5ca08f78a
noorghunaim <noor.ahmedghunaim@gmail.com>
2025-10-26 15:58:18 +0300

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -22,9 +22,9 @@ SELECT * FROM Users;
 
 -- The code to populate the Products table is as follows:
 INSERT INTO Products VALUES (1,'Laptop','Electronics',1200,10),
-...> (2,'Smartphone','Electronics',800,20),
-...> (3,'Coffee Maker','Home Appliances',150,15),
-...> (4,'Headphone','Electronics',200,25);
+(2,'Smartphone','Electronics',800,20),
+(3,'Coffee Maker','Home Appliances',150,15),
+(4,'Headphone','Electronics',200,25);
 
 -- The code to see Products table:
 SELECT * FROM Products;
@@ -32,8 +32,8 @@ SELECT * FROM Products;
 
 -- The code to populate the Orders table is as follows:
 INSERT INTO Orders VALUES (1,1,'2025-04-01',1400),
-...> (2,2,'2025-04-03',950),
-...> (3,1,'2025-04-05',200);
+(2,2,'2025-04-03',950),
+(3,1,'2025-04-05',200);
 
 -- The code to see Orders table:
 SELECT * FROM Orders;
@@ -41,10 +41,10 @@ SELECT * FROM Orders;
 
 -- The code to populate the OrderItems table is as follows:
 INSERT INTO OrderItems VALUES (1,1,1,1,1200),
-...> (2,1,4,1,200),
-...> (3,2,2,1,800),
-...> (4,2,4,1,150),
-...> (5,3,4,1,200);
+(2,1,4,1,200),
+(3,2,2,1,800),
+(4,2,4,1,150),
+(5,3,4,1,200);
 
 -- The code to see OrderItems table:
 SELECT * FROM OrderItems;
@@ -52,8 +52,8 @@ SELECT * FROM OrderItems;
 
 -- The code to populate the Reviews table is as follows:
 INSERT INTO Reviews VALUES (1,1,1,5,'Great laptop','2025-04-02'),
-...> (2,2,2,4,'Good phone','2025-04-04'),
-...> (3,3,3,3,'Average coffee maker','2025-04-06');
+(2,2,2,4,'Good phone','2025-04-04'),
+(3,3,3,3,'Average coffee maker','2025-04-06');
 
 -- The code to see Reviews table:
 SELECT * FROM Reviews;
